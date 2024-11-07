52b5a65065e83848b61e7dbf7cff4c3797b2bbaa
marcosagni98 <marcos.agni@gmail.com>
2024-11-07 20:43:33 +0100

    comments hook/users

--- src/hooks/users/useFetchUsers.ts
+++ src/hooks/users/useFetchUsers.ts
@@ -3,10 +3,20 @@ import { PaginationProps } from "../../interfaces/shared/PaginationProps";
 import { UsersTableRow } from "../../interfaces/users/UsersTableRow";
 import useFetchWithAuthAndPaginationBase from "../useFetchWithAuthAndPaginationBase";
 
+/** 
+ * Custom hook to fetch a list of users with authentication and pagination
+ * This hook simplifies the process of retrieving users' data with pagination support.
+ * @returns {Object} - Contains the data, completed status, error, and the fetch function for retrieving the user list with pagination
+ */
 const useFetchUsers = () => {
   const { data, completed, error, fetchData } =
-  useFetchWithAuthAndPaginationBase<UsersTableRow>();
+    useFetchWithAuthAndPaginationBase<UsersTableRow>();
 
+  /** 
+   * Function to fetch the list of users with pagination
+   * @param {PaginationProps} paginationProps - The pagination settings (e.g., page number, page size)
+   * @returns {Promise<any>} - Returns a promise with the response from the API containing the paginated list of users
+   */
   const fetch = (paginationProps: PaginationProps) => {
     return fetchData(`${API_BASE_URL}/api/v1/User`, paginationProps);
   };
